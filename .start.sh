6137fa14ab96debea0bc78d6625c2100f034c0eb
Megabyte Labs Automation <help@megabyte.space>
2021-06-17 04:59:10 +0000

    automation(upstream-sync) Applying changes from upstream repository.

--- .start.sh
+++ .start.sh
@@ -13,7 +13,9 @@ fi
 
 export REPO_TYPE=ansible
 git pull origin master --ff-only
-git submodule update --init --recursive
+if [ -d .git ]; then
+  git submodule update --init --recursive
+fi
 if [ ! -d "./.modules/${REPO_TYPE}" ]; then
   mkdir -p ./.modules
   git submodule add -b master https://gitlab.com/megabyte-space/common/$REPO_TYPE.git ./.modules/$REPO_TYPE
