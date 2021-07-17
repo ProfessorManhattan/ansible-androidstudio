159a75de6de6f38852a4929fcc7c5f3405902fcf
Brian Zalewski <bzalewski@manh.com>
2021-07-16 20:46:56 -0400

    Started creating Windows Docker test.

--- tests/test-windows.sh
+++ tests/test-windows.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-# WINDOWS_HOST_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
+if [ -f requirements.yml ]; then
+  ansible-galaxy install -r requirements.yml
+fi
+
+pip3 install "pywinrm>=0.3.0"
+
+ROLE_NAME=$(sed -n 's^.*- role:\ "\(.*\)"$^\1^p' test.yml)
+ln -s $(basename $PWD) ../$ROLE_NAME
+
+cp tests/ansible.cfg ansible.cfg
+cp tests/test.yml main.yml
+ansible-playbook -i tests/inventory-windows main.yml
--- tests/windows.ps1
+++ tests/windows.ps1
@@ -1,1 +1,9 @@
+Write-Host "Setting up WinRM"
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file -Verbose -EnableCredSSP -DisableBasicAuth
 
+Write-Host "Changing user password to 'AnsibleTest999'"
+$NewPassword = ConvertTo-SecureString "AnsibleTest999" –AsPlainText –Force
+Set-LocalUser –Name $env:UserName –Password $NewPassword
