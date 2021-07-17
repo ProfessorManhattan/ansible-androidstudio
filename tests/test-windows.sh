905a670bd1720a18b1226e72788bfca72d365ea1
Brian Zalewski <bzalewski@manh.com>
2021-07-16 22:36:17 -0400

    Commit

--- tests/test-windows.sh
+++ tests/test-windows.sh
@@ -4,11 +4,12 @@ if [ -f requirements.yml ]; then
   ansible-galaxy install -r requirements.yml
 fi
 
-pip3 install "pywinrm>=0.3.0"
-
 ROLE_NAME=$(sed -n 's^.*- role:\ "\(.*\)"$^\1^p' test.yml)
-ln -s $(basename $PWD) ../$ROLE_NAME
+ln -s echo "$(basename "$PWD")" "../$ROLE_NAME"
 
 cp tests/ansible.cfg ansible.cfg
 cp tests/test.yml main.yml
 ansible-playbook -i tests/inventory-windows main.yml
+
+WINDOWS_HOST_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
+echo "$WINDOWS_HOST_IP"
