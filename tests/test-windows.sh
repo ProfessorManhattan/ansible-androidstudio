e28bdaa5c1f4b03b440b8141a74c9d26ff361895
Brian Zalewski <bzalewski@manh.com>
2021-07-24 23:22:54 -0400

    Latest changes.

--- tests/test-windows.sh
+++ tests/test-windows.sh
@@ -4,11 +4,12 @@ if [ -f requirements.yml ]; then
   ansible-galaxy install -r requirements.yml
 fi
 
-ROLE_NAME=$(sed -n 's^.*- role:\ "\(.*\)"$^\1^p' tests/test.yml)
-ln -s "$(basename "$PWD")" "../$ROLE_NAME"
+ROLE_NAME=$(sed -n 's^.*- role:\ "\(.*\)"$^\1^p' test.yml)
+ln -s echo "$(basename "$PWD")" "../$ROLE_NAME"
 
-python3 -m pip install requests-credssp
-
-cp tests/ansible.cfg ~/.ansible.cfg
+cp tests/ansible.cfg ansible.cfg
 cp tests/test.yml main.yml
 ansible-playbook -i tests/inventory-windows main.yml
+
+WINDOWS_HOST_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
+echo "$WINDOWS_HOST_IP"
