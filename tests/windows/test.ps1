d9a43e4aee998497c2025eaa8ae6b83fa70702b4
Aathavan <7279940-aathavan@users.noreply.gitlab.com>
2021-09-03 10:47:36 +0000

    Update test.ps1

--- tests/windows/test.ps1
+++ tests/windows/test.ps1
@@ -1,9 +1,12 @@
-Write-Host "Setting up WinRM"
-$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
-$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
-powershell.exe -ExecutionPolicy ByPass -File $file -EnableCredSSP -DisableBasicAuth | Out-Null
+winrm get winrm/config
+
+#Write-Host "Setting up WinRM"
+#$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+#$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+#(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+#powershell.exe -ExecutionPolicy ByPass -File $file -EnableCredSSP -DisableBasicAuth | Out-Null
 
 Write-Host "Changing user password to 'AnsibleTest999'"
 $NewPassword = ConvertTo-SecureString "AnsibleTest999" -AsPlainText -Force
 Set-LocalUser -Name $env:UserName -Password $NewPassword
+Write-Host "Changed..."
