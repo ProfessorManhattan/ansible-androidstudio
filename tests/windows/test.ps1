73cfcd51b5c97641c4aebde766d464a521c3bf2b
Aathavan <7279940-aathavan@users.noreply.gitlab.com>
2021-09-03 10:55:01 +0000

    Update test.ps1

--- tests/windows/test.ps1
+++ tests/windows/test.ps1
@@ -1,4 +1,7 @@
 winrm get winrm/config
+Get-Service WinRM
+
+docker images
 
 #Write-Host "Setting up WinRM"
 #$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
@@ -10,3 +13,10 @@ Write-Host "Changing user password to 'AnsibleTest999'"
 $NewPassword = ConvertTo-SecureString "AnsibleTest999" -AsPlainText -Force
 Set-LocalUser -Name $env:UserName -Password $NewPassword
 Write-Host "Changed..."
+
+Write-Host "Running the Ansible play on the local machine via a Docker container with Ansible"
+$CurrentLocation = Get-Location
+$WorkDirectory = Split-Path -leaf -path (Get-Location)
+$HostIP = (Get-NetIPConfiguration | Where-Object -Property IPv4DefaultGateway).IPv4Address.IPAddress
+docker run -it -v $("$($CurrentLocation)"+':/'+$WorkDirectory) -w $('/'+$WorkDirectory) `
+--add-host='windows-docker:'$HostIP --entrypoint /bin/sh megabytelabs/ansible:slim ./tests/windows/test.sh
