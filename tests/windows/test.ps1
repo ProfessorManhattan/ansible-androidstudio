65567fb3f22fbf115d09044bb654cb57c03f8c27
Aathavan <7279940-aathavan@users.noreply.gitlab.com>
2021-09-03 10:34:48 +0000

    Update test.ps1

--- tests/windows/test.ps1
+++ tests/windows/test.ps1
@@ -2,7 +2,7 @@ Write-Host "Setting up WinRM"
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
-powershell.exe -ExecutionPolicy ByPass -File $file -Verbose -EnableCredSSP -DisableBasicAuth
+powershell.exe -ExecutionPolicy ByPass -File $file -EnableCredSSP -DisableBasicAuth | Out-Null
 
 Write-Host "Changing user password to 'AnsibleTest999'"
 $NewPassword = ConvertTo-SecureString "AnsibleTest999" -AsPlainText -Force
