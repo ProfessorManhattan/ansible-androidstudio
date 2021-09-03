ab735f5787227c61fd1dfe921e6b1b67f07da229
Aathavan <7279940-aathavan@users.noreply.gitlab.com>
2021-09-03 11:04:13 +0000

    Update test.ps1

--- tests/windows/test.ps1
+++ tests/windows/test.ps1
@@ -3,20 +3,23 @@ Get-Service WinRM
 
 docker images
 
-#Write-Host "Setting up WinRM"
+#Write-Output "Setting up WinRM"
 #$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 #$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 #(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 #powershell.exe -ExecutionPolicy ByPass -File $file -EnableCredSSP -DisableBasicAuth | Out-Null
 
-Write-Host "Changing user password to 'AnsibleTest999'"
+Write-Output "Changing user password to 'AnsibleTest999'"
 $NewPassword = ConvertTo-SecureString "AnsibleTest999" -AsPlainText -Force
 Set-LocalUser -Name $env:UserName -Password $NewPassword
-Write-Host "Changed..."
+Write-Output "Changed..."
 
-Write-Host "Running the Ansible play on the local machine via a Docker container with Ansible"
+Write-Output "Running the Ansible play on the local machine via a Docker container with Ansible"
 $CurrentLocation = Get-Location
 $WorkDirectory = Split-Path -leaf -path (Get-Location)
 $HostIP = (Get-NetIPConfiguration | Where-Object -Property IPv4DefaultGateway).IPv4Address.IPAddress
+Write-Output $HostIP
+docker run hello-world
+
 docker run -it -v $("$($CurrentLocation)"+':/'+$WorkDirectory) -w $('/'+$WorkDirectory) `
 --add-host='windows-docker:'$HostIP --entrypoint /bin/sh megabytelabs/ansible:slim ./tests/windows/test.sh
