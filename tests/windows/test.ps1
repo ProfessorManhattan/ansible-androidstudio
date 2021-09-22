cf13e48d1fba72ea165b0010337248188b0d6f5d
Megabyte Labs Automation <help@megabyte.space>
2021-09-22 01:34:20 +0000

    automation(upstream-sync) Applying changes from upstream repository.

--- tests/windows/test.ps1
+++ tests/windows/test.ps1
@@ -25,15 +25,14 @@ Write-Host "Changing $env:UserName password to 'AnsibleTest999'"
 $NewPassword = ConvertTo-SecureString "AnsibleTest999" -AsPlainText -Force
 Set-LocalUser -Name $env:UserName -Password $NewPassword
 
-Write-Host "Setting up WinRM CredSSP"
-$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
-$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
-PowerShell.exe -ExecutionPolicy ByPass -File $file -Verbose -EnableCredSSP -DisableBasicAuth
+# Write-Host "Setting up WinRM CredSSP"
+# $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+# $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+# (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+# PowerShell.exe -ExecutionPolicy ByPass -File $file -Verbose -EnableCredSSP -DisableBasicAuth
 
-Write-Host "Running the Ansible play on the local machine via a Docker container with Ansible"
-$CurrentLocation = Get-Location
-$WorkDirectory = Split-Path -leaf -path (Get-Location)
-$HostIP = (Get-NetIPConfiguration | Where-Object -Property IPv4DefaultGateway).IPv4Address.IPAddress
-docker run -it -v $("$($CurrentLocation)"+':/'+$WorkDirectory) -w $('/'+$WorkDirectory) `
---add-host='windows-docker:'$HostIP --entrypoint /bin/sh megabytelabs/ansible:slim ./tests/windows/test.sh
+# Write-Host "Running the Ansible play on the local machine via a Docker container with Ansible"
+# $CurrentLocation = Get-Location
+# $WorkDirectory = Split-Path -leaf -path (Get-Location)
+# $HostIP = (Get-NetIPConfiguration | Where-Object -Property IPv4DefaultGateway).IPv4Address.IPAddress
+# docker run -v $("$($CurrentLocation)"+':/'+$WorkDirectory) -w $('/'+$WorkDirectory) --add-host='windows-docker:'$HostIP --entrypoint /bin/sh megabytelabs/ansible:slim ./tests/windows/test.sh
