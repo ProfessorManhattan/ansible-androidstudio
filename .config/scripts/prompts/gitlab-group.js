ef059e019eed5616b9129c1fdfe237f380208687
Megabyte Labs Automation <help@megabyte.space>
2021-11-28 20:59:56 +0000

    automation(upstream-sync) Applying changes from upstream repository.

--- .config/scripts/prompts/gitlab-group.js
+++ .config/scripts/prompts/gitlab-group.js
@@ -1,7 +1,9 @@
 import inquirer from 'inquirer'
 import { execSync } from 'node:child_process'
-import { writeFileSync } from 'node:fs'
+import { existsSync, writeFileSync } from 'node:fs'
 import { logInstructions } from './lib/log.js'
+
+const userScript = '.cache/gitlab-group-script.sh'
 
 /**
  * Prompts the user for the GitLab group they wish to run bulk commands on.
@@ -18,6 +20,23 @@ async function promptForGroup() {
   ])
 
   return response.groupURL.replace('https://gitlab.com/', '').replace('gitlab.com/', '')
+}
+
+/**
+ * If a script was already created, ask if the user would like to re-use it
+ *
+ * @returns {string} A boolean answer, true if they want to re-use the script
+ */
+async function promptForRecycle() {
+  const response = await inquirer.prompt([
+    {
+      message: 'You already created a script (located in .config/gitlab-group-script.sh). Would you like to re-use it?',
+      name: 'reuseScript',
+      type: 'confirm'
+    }
+  ])
+
+  return response.reuseScript
 }
 
 /**
@@ -38,6 +57,15 @@ async function promptForScript() {
 }
 
 /**
+ * Opens the default editor and then saves the file
+ * to .cache/gitlab-group-script.sh
+ */
+async function scriptPrompt() {
+  const script = await promptForScript()
+  writeFileSync(userScript, script)
+}
+
+/**
  * Main script logic
  */
 async function run() {
@@ -48,8 +76,15 @@ async function run() {
       ' write/paste a bash script.'
   )
   const choice = await promptForGroup()
-  const script = await promptForScript()
-  writeFileSync('.cache/gitlab-group-script.sh', script)
+
+  if (existsSync(userScript)) {
+    const reuse = await promptForRecycle()
+    if (!reuse) {
+      await scriptPrompt()
+    }
+  } else {
+    await scriptPrompt()
+  }
   execSync(`task git:gitlab:group:exec:cli -- ${choice} ---- 'bash ${process.cwd()}/.cache/gitlab-group-script.sh'`, {
     stdio: 'inherit'
   })
