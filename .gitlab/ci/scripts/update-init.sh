4ba0d938ff7a817f535b606433ec650fe65a9a09
Megabyte Labs Automation <help@megabyte.space>
2021-11-29 10:29:33 +0000

    automation(upstream-sync) Applying changes from upstream repository.

--- .gitlab/ci/scripts/update-init.sh
+++ .gitlab/ci/scripts/update-init.sh
@@ -21,7 +21,7 @@ fi
 
 # @description Clone shared files repository
 rm -rf common-shared
-git clone https://gitlab.com/megabyte-labs/common/shared.git common-shared
+git clone --depth=1 https://gitlab.com/megabyte-labs/common/shared.git common-shared
 
 # @description Refresh taskfiles and GitLab CI files
 mkdir -p .config
@@ -32,18 +32,34 @@ rm -rf .gitlab/ci
 cp -rT common-shared/common/.gitlab/ci .gitlab/ci
 
 # @description Ensure proper NPM dependencies are installed
-npm install --save-dev @mblabs/eslint-config@latest
-npm install --save-optional chalk inquirer signale string-break
+echo "Installing NPM packages"
+if [ ! -f 'package.json' ]; then
+  echo "{}" > package.json
+fi
+npm install --save-dev --ignore-scripts @mblabs/eslint-config@latest @mblabs/prettier-config@latest
+npm install --save-optional --ignore-scripts chalk inquirer signale string-break
 
 # @description Re-generate the Taskfile.yml if it has invalid includes
+echo "Ensuring Taskfile is properly configured"
 if ! task donothing &> /dev/null; then
   curl -s https://gitlab.com/megabyte-labs/common/shared/-/raw/master/Taskfile.yml > Taskfile-shared.yml
   TMP="$(mktemp)"
   yq eval-all 'select(fileIndex==0).includes = select(fileIndex==1).includes | select(fileIndex==0)' Taskfile.yml Taskfile-shared.yml > "$TMP"
   mv "$TMP" Taskfile.yml
   rm Taskfile-shared.yml
-  npm install
-  task fix:eslint -- Taskfile.yml
+  npm install --ignore-scripts
+  if ! task fix:eslint -- Taskfile.yml; then
+    curl -s https://gitlab.com/megabyte-labs/common/shared/-/raw/master/update/package-requirements.json > package-requirements.json
+    if ! type jq &> /dev/null; then
+      echo "ERROR: jq must be installed"
+      exit 1
+    else
+      TMP="$(mktemp)"
+      jq -s '.[0] * .[1]' package.json package-requirements.json > "$TMP"
+      mv "$TMP" package.json
+    fi
+    rm package-requirements.json
+  fi
 fi
 
 # @description Clean up
